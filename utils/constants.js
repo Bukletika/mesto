84b3fa73016e6dea1709d1574aff3045446ef4be
Anton Sedin <bukleting@yandex.ru>
2021-04-21 19:42:39 +0700

    change constants image path

--- utils/constants.js
+++ utils/constants.js
@@ -2,27 +2,27 @@
 export const initialCards = [
   {
     name: 'Лондон',
-    link: '../images/london.jpg'
+    link: './images/london.jpg'
   },
   {
     name: 'Нью-Йорк',
-    link: '../images/newyork.jpg'
+    link: './images/newyork.jpg'
   },
   {
     name: 'Прага',
-    link: '../images/praha.jpg'
+    link: './images/praha.jpg'
   },
   {
     name: 'Венеция',
-    link: '../images/venecia.jpg'
+    link: './images/venecia.jpg'
   },
   {
     name: 'Барселона',
-    link: '../images/barselona.jpg'
+    link: './images/barselona.jpg'
   },
   {
     name: 'Париж',
-    link: '../images/paris.jpg'
+    link: './images/paris.jpg'
   }
 ];
 
